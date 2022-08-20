736d2ea4c6b3a18d2dfaba93eb535d5759ec6248
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2022-08-20 14:17:59 -0400

    Update shift.rs

--- src/chips/cpu8086/instructions/shift.rs
+++ src/chips/cpu8086/instructions/shift.rs
@@ -1,64 +1,69 @@
 use super::super::CPU;
 
 use super::super::definitions::operand;
-use super::super::definitions::general;
 
 use log::Level::Trace;
 use log::{trace, log_enabled};
 
-pub fn shr_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn shr_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let result = cpu.flags.shr_ror_rcr_byte(set_val, get_val);
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn shr_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn shr_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let result = cpu.flags.shr_ror_rcr_word(set_val, get_val);
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
-pub fn sar_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn sar_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shr_ror_rcr_byte(set_val, get_val);
   if (set_val as i8) < 0 {
     result |= 0b1000_0000;
   }
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn sar_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn sar_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shr_ror_rcr_word(set_val, get_val);
   if (set_val as i8) < 0 {
     result |= 0b1000_0000;
   }
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
-pub fn ror_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn ror_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shr_ror_rcr_byte(set_val, get_val);
   if cpu.flags.carry {
     result |= 0b1000_0000;
   }
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn ror_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn ror_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shr_ror_rcr_word(set_val, get_val);
   if cpu.flags.carry {
     result |= 0b1000_0000;
   }
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
-pub fn rcr_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn rcr_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let old_carry = cpu.flags.carry;
   let mut result = cpu.flags.shr_ror_rcr_byte(set_val, get_val);
@@ -66,9 +71,10 @@ pub fn rcr_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
     result |= 0b1000_0000;
   }
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn rcr_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn rcr_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHR {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let old_carry = cpu.flags.carry;
   let mut result = cpu.flags.shr_ror_rcr_word(set_val, get_val);
@@ -76,44 +82,49 @@ pub fn rcr_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
     result |= 0b1000_0000;
   }
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
 
 
-pub fn shl_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHL {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn shl_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let result = cpu.flags.shl_rol_rcl_byte(set_val, get_val);
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn shl_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: SHL {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn shl_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: SHL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let result = cpu.flags.shl_rol_rcl_word(set_val, get_val);
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
-pub fn rol_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: ROL {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn rol_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: ROL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shl_rol_rcl_byte(set_val, get_val);
   if cpu.flags.carry {
     result |= 1;
   }
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn rol_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: ROL {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn rol_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: ROL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let mut result = cpu.flags.shl_rol_rcl_word(set_val, get_val);
   if cpu.flags.carry {
     result |= 1;
   }
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
 
-pub fn rcl_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: RCL {}, {}", cpu.current_address, general::label_byte(&set_op), general::label_byte(&get_op)); }
+pub fn rcl_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: RCL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_byte(&set_op), cpu.read_byte(&get_op));
   let old_carry = cpu.flags.carry;
   let mut result = cpu.flags.shl_rol_rcl_byte(set_val, get_val);
@@ -121,9 +132,10 @@ pub fn rcl_byte(cpu: &mut CPU, set_op: operand::Byte, get_op: operand::Byte) {
     result |= 1;
   }
   cpu.write_byte(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
-pub fn rcl_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
-  if log_enabled!(Trace) { trace!("{:05X}: RCL {}, {}", cpu.current_address, general::label_word(&set_op), general::label_byte(&get_op)); }
+pub fn rcl_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) -> usize {
+  if log_enabled!(Trace) { trace!("{:05X}: RCL {}, {}", cpu.current_address, set_op.label(), get_op.label()); }
   let (set_val, get_val) = (cpu.read_word(&set_op), cpu.read_byte(&get_op));
   let old_carry = cpu.flags.carry;
   let mut result = cpu.flags.shl_rol_rcl_word(set_val, get_val);
@@ -131,4 +143,5 @@ pub fn rcl_word(cpu: &mut CPU, set_op: operand::Word, get_op: operand::Byte) {
     result |= 1;
   }
   cpu.write_word(&set_op, result);
+  set_op.get_rotate_cycles(get_op)
 }
